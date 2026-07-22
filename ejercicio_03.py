0d2f9cfe67fbbe2c5d881e5e2c56ac9df4c160a7
rojascastilloignacio8-ops <rojascastilloignacio8@gmail.com>
2026-07-22 10:26:08 -0400

    Se ha agregado un endpoint

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/productos")
+async def obtenerProducto():
+    return [ 
+        {"id": 1, "Descripcion": "Electronicos", "Producto": "Televisor"},
+        {"id": 2, "Descripcion": "Casa y jardin", "Producto": "Herbicida"},
+        {"id": 3, "Descripcion": "Comida", "Producto": "Pan integral Bimbo"} 
+        ]
